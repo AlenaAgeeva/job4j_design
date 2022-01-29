e6c23afcf2262053f7a30618424500875225bbfb
AlenaAgeeva <alenapvageeva@gmail.com>
2022-01-29 14:21:48 -0400

    2. Создать контейнер на базе связанного списка [#159]

---
+++ src/main/java/ru/job4j/collection/list/SimpleLinkedList.java
@@ -0,0 +1,75 @@
+package ru.job4j.collection.list;
+
+import java.util.*;
+
+public class SimpleLinkedList<E> implements List<E> {
+    private int size;
+    private int modCount;
+    private Node first;
+    private Node last;
+
+    private static class Node<E> {
+        E element;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.prev = prev;
+            this.element = element;
+            this.next = next;
+        }
+    }
+
+    @Override
+    public void add(E value) {
+        Node<E> l = last;
+        Node<E> newNode = new Node<>(l, value, null);
+        last = newNode;
+        if (l == null) {
+            first = newNode;
+        } else {
+            l.next = newNode;
+        }
+        modCount++;
+        size++;
+    }
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        Node<E> current = first;
+        int count = 0;
+        while (count != index) {
+            current = current.next;
+            count++;
+        }
+        return current.element;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private final int expectedModCount = modCount;
+            private int count = 0;
+            Node<E> node = first;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return count < size;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                node = (count == 0) ? first : node.next;
+                count++;
+                return node.element;
+            }
+        };
+    }
+}
