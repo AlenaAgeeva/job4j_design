bb67ac816bc7d4369ca1478f67de14e3416a30a2
AlenaAgeeva <alenapvageeva@gmail.com>
2022-04-15 15:34:29 -0300

    5. Переопределить и equals и hashCode [#1002]

--- src/test/java/ru/job4j/collection/list/map/User.java
+++ src/test/java/ru/job4j/collection/list/map/User.java
@@ -28,6 +28,14 @@ public class User {
                 && birthday.equals(user.birthday);
     }
 
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + Integer.hashCode(children);
+        result = 31 * result + birthday.hashCode();
+        return result;
+    }
+
     public static void main(String[] args) {
         User userOne = new User("Tom", 2, new GregorianCalendar(1986, 5, 11));
         User userTwo = new User("Tom", 2, new GregorianCalendar(1986, 5, 11));
