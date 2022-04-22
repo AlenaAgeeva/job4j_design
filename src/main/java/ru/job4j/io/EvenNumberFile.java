41ac468f952fb55c5bbc1d27ae461eb4232be912
AlenaAgeeva <alenapvageeva@gmail.com>
2022-04-22 13:29:45 -0300

    0.2. FileInputStream [#4898]

---
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -0,0 +1,21 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        try (FileInputStream in = new FileInputStream("even.txt")) {
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                if (Character.getNumericValue(read) % 2 == 0) {
+                    text.append((char) read);
+                }
+            }
+            System.out.println(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
