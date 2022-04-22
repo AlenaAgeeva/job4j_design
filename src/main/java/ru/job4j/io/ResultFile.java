8ecf5a36436322a5f77a89602a39880fd843458e
AlenaAgeeva <alenapvageeva@gmail.com>
2022-04-22 11:06:59 -0300

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,19 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("file.txt")) {
+            for (int i = 1; i <= 9; i++) {
+                for (int j = 1; j <= 9; j++) {
+                    String rsl = i + " * " + j + " = " + i * j;
+                    out.write(rsl.getBytes());
+                    out.write(System.lineSeparator().getBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
