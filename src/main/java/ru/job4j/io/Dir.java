c9ac121b5d3cc0c0d2a34a015f86dee29a78aacd
AlenaAgeeva <alenapvageeva@gmail.com>
2022-04-29 19:57:47 -0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -1,12 +1,15 @@
 package ru.job4j.io;
 
-import java.io.*;
+import java.io.File;
 import java.util.Arrays;
 import java.util.Objects;
 
 public class Dir {
     public static void main(String[] args) {
-        File file = new File("c:\\projects");
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+        File file = new File(args[0]);
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
@@ -20,6 +23,5 @@ public class Dir {
         }
         Arrays.stream(Objects.requireNonNull(file.listFiles()))
                 .forEach(s -> System.out.printf("Name: %s, size: %s%n", s.getName(), s.getAbsoluteFile().length()));
-
     }
 }
--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -7,11 +7,21 @@ import java.util.function.*;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get("c:\\projects");
+        new Search().validation(args);
+        Path start = Paths.get(args[0]);
         search(start, p -> p.toFile()
                 .getName()
-                .endsWith(".js"))
+                .endsWith(args[1]))
                 .forEach(System.out::println);
+    }
+
+    public void validation(String[] arg) {
+        if (arg.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar search.jar ROOT_FOLDER.");
+        }
+        if (!Paths.get(arg[0]).toFile().exists()) {
+            throw new IllegalArgumentException("File does not exit.");
+        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
