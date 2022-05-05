58cde6ba7bc13f664930baf376b2532476f9dbb1
AlenaAgeeva <alenapvageeva@gmail.com>
2022-05-05 12:03:28 -0300

    6. Кодировка. [#862 #297469]

--- src/main/java/ru/job4j/io/ConsoleChat.java
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -11,6 +11,7 @@ public class ConsoleChat {
     private static final String CONTINUE = "продолжить";
     private final String path;
     private final String botAnswers;
+    private static String userPhrase;
 
     public ConsoleChat(String path, String botAnswers) {
         this.path = path;
@@ -19,37 +20,41 @@ public class ConsoleChat {
 
     public void run() {
         try (BufferedReader b = new BufferedReader(new InputStreamReader(System.in))) {
-            String userPhrase = b.readLine();
             List<String> botPh = readPhrases();
             List<String> log = new ArrayList<>();
             boolean canRead = true;
-            while (!OUT.equals(userPhrase)) {
+            while (!OUT.equals(updateUserPhrase(b))) {
                 String s = botPh.get(new Random().nextInt(botPh.size()));
                 if (canRead) {
-                    if (!STOP.equals(userPhrase)) {
+                    if (STOP.equals(userPhrase)) {
+                        canRead = false;
+                        log.add("User: " + userPhrase);
+                        continue;
+                    }
+                    log.add("User: " + userPhrase);
+                    log.add("Bot: " + s);
+                    System.out.println(s);
+                } else {
+                    if (CONTINUE.equals(userPhrase)) {
+                        canRead = true;
                         log.add("User: " + userPhrase);
                         log.add("Bot: " + s);
                         System.out.println(s);
                         continue;
                     }
-                    canRead = false;
                     log.add("User: " + userPhrase);
-                } else {
-                    if (!CONTINUE.equals(userPhrase)) {
-                        log.add("User: " + userPhrase);
-                        continue;
-                    }
-                    canRead = true;
-                    log.add("User: " + userPhrase);
-                    log.add("Bot: " + s);
-                    System.out.println(s);
                 }
-                userPhrase = b.readLine();
             }
+            log.add(OUT);
             saveLog(log);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private String updateUserPhrase(BufferedReader br) throws IOException {
+        userPhrase = br.readLine();
+        return userPhrase;
     }
 
     private List<String> readPhrases() {
