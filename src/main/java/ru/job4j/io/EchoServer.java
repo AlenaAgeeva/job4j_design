ed18fbb2f03fd92513fcae8ba101fe31fcd4add2
AlenaAgeeva <alenapvageeva@gmail.com>
2022-05-10 14:50:21 -0300

    4. Slf4j - вывод exception. [#268853 #299516]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -7,7 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class EchoServer {
-    public static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+    public static final Logger LOG = LoggerFactory.getLogger(EchoServer.class.getName());
 
     public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
