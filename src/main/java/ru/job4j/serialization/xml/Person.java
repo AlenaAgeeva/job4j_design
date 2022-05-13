a8c0f577358cb6dc861ce6fb28c9c5273ea4c8d5
AlenaAgeeva <alenapvageeva@gmail.com>
2022-05-13 10:31:40 -0300

    4. JAXB. Преобразование XML в POJO. [#315063]

---
+++ src/main/java/ru/job4j/serialization/xml/Person.java
@@ -0,0 +1,65 @@
+package ru.job4j.serialization.xml;
+
+import javax.xml.bind.*;
+import javax.xml.bind.annotation.*;
+import java.io.*;
+import java.util.*;
+
+@XmlRootElement(name = "person")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Person {
+    @XmlAttribute
+    private boolean sex;
+
+    @XmlAttribute
+    private int age;
+
+    private Contact contact;
+    @XmlElementWrapper
+    @XmlElement(name = "status")
+    private String[] statuses;
+
+    public Person() {
+    }
+
+    public Person(boolean sex, int age, Contact contact, String... statuses) {
+        this.sex = sex;
+        this.age = age;
+        this.contact = contact;
+        this.statuses = statuses;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{"
+                + "sex=" + sex
+                + ", age=" + age
+                + ", contact=" + contact
+                + ", statuses=" + Arrays.toString(statuses)
+                + '}';
+    }
+
+    public static void main(String[] args) throws JAXBException {
+
+        final Person person = new Person(false, 30, new Contact("11-111"), "Worker", "Married");
+
+        JAXBContext context = JAXBContext.newInstance(Person.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        String xml = "";
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(person, writer);
+            xml = writer.getBuffer().toString();
+            System.out.println(xml);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(xml)) {
+            Person result = (Person) unmarshaller.unmarshal(reader);
+            System.out.println(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
