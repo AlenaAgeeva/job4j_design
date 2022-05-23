fd61383fcd8e272a7bc4d59717b680f58b1f0609
AlenaAgeeva <alenapvageeva@gmail.com>
2022-05-23 13:04:35 -0300

    0.1. Связи между таблицами [#373615 #303346]

--- data/many_to_many.sql
+++ data/many_to_many.sql
@@ -2,24 +2,43 @@ create table subject(
 	id serial primary key,
 	name varchar(55)
 );
-create table grade(
-	id serial primary key,
-	name varchar(55)
-);
 create table student(
 	id serial primary key,
-	name varchar(255),
-	subject_id int references subject(id),
-	grade_id int references grade(id)
+	first_name varchar(255),
+	last_name varchar(255)
+
+);
+create table rating(
+	id serial primary key,
+	assessment char(1)
+);
+create table study_class(
+	id serial primary key,
+	student_id int references student(id),
+	subject_id int references subject (id),
+	rating_id int references rating (id)
 );
 insert into subject (name) values ('Mathematics');
 insert into subject (name) values ('Physics');
 insert into subject (name) values ('Chemistry');
-insert into grade (name) values ('A');
-insert into grade (name) values ('B');
-insert into grade (name) values ('C');
-insert into grade (name) values ('D');
-insert into student (name,subject_id,grade_id) values ('Sam',1,3);
-insert into student (name,subject_id,grade_id) values ('Tom',2,1);
-insert into student (name,subject_id,grade_id) values ('Will',3,2);
-insert into student (name,subject_id,grade_id) values ('Kris',1,1);
+insert into subject (name) values ('Literature');
+insert into student(first_name,last_name) values('Tom','Smith');
+insert into student(first_name,last_name) values('Jon','Black');
+insert into student(first_name,last_name) values('Sam','Button');
+insert into student(first_name,last_name) values('Will','Drake');
+insert into rating(assessment) values('A');
+insert into rating(assessment) values('B');
+insert into rating(assessment) values('C');
+insert into rating(assessment) values('D');
+insert into study_class(student_id,subject_id,rating_id) values(1,1,1);
+insert into study_class(student_id,subject_id,rating_id) values(1,2,2);
+insert into study_class(student_id,subject_id,rating_id) values(1,3,3);
+insert into study_class(student_id,subject_id,rating_id) values(1,4,4);
+insert into study_class(student_id,subject_id,rating_id) values(2,2,2);
+insert into study_class(student_id,subject_id,rating_id) values(2,3,4);
+insert into study_class(student_id,subject_id,rating_id) values(2,1,2);
+insert into study_class(student_id,subject_id,rating_id) values(3,2,2);
+insert into study_class(student_id,subject_id,rating_id) values(3,3,1);
+insert into study_class(student_id,subject_id,rating_id) values(4,4,4);
+insert into study_class(student_id,subject_id,rating_id) values(4,1,2);
+
