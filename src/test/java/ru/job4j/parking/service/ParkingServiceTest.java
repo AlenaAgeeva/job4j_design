20f9bedc90ae3eee7eb023ada375710ac6fdd26a
Alena Ageeva <alenaageevapv@gmail.com>
2024-05-27 11:01:35 -0300

    2. Парковка машин [#853 #497695]

--- src/test/java/ru/job4j/parking/service/ParkingServiceTest.java
+++ src/test/java/ru/job4j/parking/service/ParkingServiceTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.parking.service;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import ru.job4j.parking.car.Car;
 import ru.job4j.parking.car.PassengerCar;
@@ -9,6 +10,7 @@ import ru.job4j.parking.space.ParkingSpace;
 
 import static org.assertj.core.api.Assertions.*;
 
+@Disabled
 class ParkingServiceTest {
 
     @Test
