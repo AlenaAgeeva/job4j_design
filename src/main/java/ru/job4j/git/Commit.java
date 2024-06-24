dcf8dd2c81b188bb6cb86fbdbf6b1d9e1a97eca7
Alena Ageeva <alenaageevapv@gmail.com>
2024-06-24 20:40:19 -0300

    Тестовый коммит.

---
+++ src/main/java/ru/job4j/git/Commit.java
@@ -0,0 +1,19 @@
+package ru.job4j.git;
+
+public class Commit {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int substraction(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplication(int a, int b) {
+        return a * b;
+    }
+
+    public int division(int a, int b) {
+        return a / b;
+    }
+}
